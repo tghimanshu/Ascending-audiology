62b66b29942fed9747ee289001b2dc07e1ff0bb5
Himanshu <himanshugohil234@gmail.com>
2020-12-31 15:14:28 +0530

    added remove_points and other graph related functionality

--- main.onefile.py
+++ main.onefile.py
@@ -12,7 +12,13 @@ points = {
     'red_open_bracket':[],
     'blue_close_bracket':[],
     'red_sq_bkt':[],
-    'blue_sq_bkt':[]
+    'blue_sq_bkt': [],   
+    'red_circle_nr':[],
+    'blue_X_nr':[],
+    'red_open_bracket_nr':[],
+    'blue_close_bracket_nr':[],
+    'red_sq_bkt_nr':[],
+    'blue_sq_bkt_nr':[]
 }
 
 points_count = {
@@ -21,7 +27,13 @@ points_count = {
     'red_open_bracket':0,
     'blue_close_bracket':0,
     'red_sq_bkt':0,
-    'blue_sq_bkt':0
+    'blue_sq_bkt':0,  
+    'red_circle_nr':0,
+    'blue_X_nr':0,
+    'red_open_bracket_nr':0,
+    'blue_close_bracket_nr':0,
+    'red_sq_bkt_nr':0,
+    'blue_sq_bkt_nr':0
 }
 
 
@@ -67,16 +79,43 @@ def check_y_for_same(arg, x_chord, y_chord):
     points[arg] = temp_items
     return exists_any
 
+def remove_points(e):
+    # ex = e.x
+    # ey = e.y
+    t = c.find_withtag('point')
+    ex,ey = get_oval_centre(c.bbox(t))
+    for arg in points.keys():
+        temp = []
+        for i in points[arg]:
+            print(ex, ey, i[0], i[1])
+            if (i[0] == ex and i[1] == ey):
+                c.delete(i[2])
+                continue
+            temp.append(i)
+        points[arg] = temp
+    create_graph_lines('red_circle')
+
 def create_graph_lines(arg):
     # t = c.find()
     c.delete('lines')
+
+    temp_points = {
+        'red_circle': points['red_circle']+points['red_circle_nr'],
+         'blue_X': points['blue_X']+points['blue_X_nr'],
+         'red_open_bracket':points['red_open_bracket']+points['red_open_bracket_nr'],
+         'blue_close_bracket': points['blue_close_bracket']+points['blue_close_bracket_nr'],
+         'red_sq_bkt': points['red_sq_bkt']+points['red_sq_bkt_nr'],
+         'blue_sq_bkt': points['blue_sq_bkt']+points['blue_sq_bkt_nr']
+         }
     
-    lines = sorted(points[arg], key=lambda item: item[0])
-    for i, p in enumerate(lines):
-        try:
-            c.create_line(p[0], p[1], lines[i + 1][0], lines[i + 1][1], fill='red', tags='lines')
-        except IndexError:
-            pass
+    for arg in temp_points.keys():
+        lines = sorted(temp_points[arg], key=lambda item: item[0])
+        for i, p in enumerate(lines):
+            try:
+                if ('_nr' not in lines[i][2] and '_nr' not in lines[i+1][2]):
+                    c.create_line(p[0], p[1], lines[i + 1][0], lines[i + 1][1], fill='red', tags='lines')
+            except IndexError:
+                pass
 
 
 def evn(e, arg):
@@ -120,6 +159,44 @@ def evn(e, arg):
         points[arg].append([oc[0], oc[1], arg+str(points_count[arg])])
         points_count[arg]+=1
         create_graph_lines(arg)
+    # No Response
+    if (arg == 'red_circle_nr'):   
+        ex = check_y_for_same(arg, oc[0], oc[1])
+        c.create_image(oc[0], oc[1], image=red_circle_nr, tags=arg+str(points_count[arg]))
+        points[arg].append([oc[0], oc[1], arg+str(points_count[arg])])
+        points_count[arg]+=1
+        create_graph_lines(arg)
+    if(arg == 'blue_X_nr'):
+        check_y_for_same(arg, oc[0], oc[1])
+        c.create_image(oc[0], oc[1], image=blue_X_nr, tags=arg+str(points_count[arg]))
+        points[arg].append([oc[0], oc[1], arg+str(points_count[arg])])
+        points_count[arg]+=1
+        create_graph_lines(arg)
+    if(arg == 'red_open_bracket_nr'):
+        check_y_for_same(arg, oc[0], oc[1])
+        c.create_image(oc[0], oc[1], image=red_open_bracket_nr, tags=arg+str(points_count[arg]))
+        points[arg].append([oc[0], oc[1], arg+str(points_count[arg])])
+        points_count[arg]+=1
+        create_graph_lines(arg)
+    if(arg == 'blue_close_bracket_nr'):
+        check_y_for_same(arg, oc[0], oc[1])
+        c.create_image(oc[0], oc[1], image=blue_close_bracket_nr, tags=arg+str(points_count[arg]))
+        points[arg].append([oc[0], oc[1], arg+str(points_count[arg])])
+        points_count[arg]+=1
+        create_graph_lines(arg)
+    if(arg == 'red_sq_bkt_nr'):    
+        check_y_for_same(arg, oc[0], oc[1])
+        c.create_image(oc[0], oc[1], image=red_sq_bkt_nr, tags=arg+str(points_count[arg]))
+        points[arg].append([oc[0], oc[1], arg+str(points_count[arg])])
+        points_count[arg]+=1
+        create_graph_lines(arg)
+    if(arg == 'blue_sq_bkt_nr'):
+        check_y_for_same(arg, oc[0], oc[1])
+        c.create_image(oc[0], oc[1], image=blue_sq_bkt_nr, tags=arg+str(points_count[arg]))
+        points[arg].append([oc[0], oc[1], arg+str(points_count[arg])])
+        points_count[arg]+=1
+        create_graph_lines(arg)
+
 
 red_circle = Image.open("images/red_circle.png")
 red_circle = red_circle.resize((15, 15), Image.ANTIALIAS)
@@ -145,11 +222,36 @@ blue_sq_bkt = Image.open("images/blue_sq_bkt.png")
 blue_sq_bkt = blue_sq_bkt.resize((15, 15), Image.ANTIALIAS)
 blue_sq_bkt = ImageTk.PhotoImage(blue_sq_bkt)
 
-def insert_image(arg):
-    c.bind('<Button-1>', lambda event, arg=arg: evn(event,  arg))
+# No Response
+
+
+red_circle_nr = Image.open("images/red_circle_nr.png")
+red_circle_nr = red_circle_nr.resize((30, 30), Image.ANTIALIAS)
+red_circle_nr = ImageTk.PhotoImage(red_circle_nr)
+
+blue_X_nr = Image.open("images/blue_X_nr.png")
+blue_X_nr = blue_X_nr.resize((30, 30), Image.ANTIALIAS)
+blue_X_nr = ImageTk.PhotoImage(blue_X_nr)
+
+red_open_bracket_nr = Image.open("images/red_open_bracket_nr.png")
+red_open_bracket_nr = red_open_bracket_nr.resize((30, 30), Image.ANTIALIAS)
+red_open_bracket_nr = ImageTk.PhotoImage(red_open_bracket_nr)
+
+blue_close_bracket_nr = Image.open("images/blue_close_bracket_nr.png")
+blue_close_bracket_nr = blue_close_bracket_nr.resize((30, 30), Image.ANTIALIAS)
+blue_close_bracket_nr = ImageTk.PhotoImage(blue_close_bracket_nr)
+
+red_sq_bkt_nr = Image.open("images/red_sq_bkt_nr.png")
+red_sq_bkt_nr = red_sq_bkt_nr.resize((40, 40), Image.ANTIALIAS)
+red_sq_bkt_nr = ImageTk.PhotoImage(red_sq_bkt_nr)
+
+blue_sq_bkt_nr = Image.open("images/blue_sq_bkt_nr.png")
+blue_sq_bkt_nr = blue_sq_bkt_nr.resize((40, 40), Image.ANTIALIAS)
+blue_sq_bkt_nr = ImageTk.PhotoImage(blue_sq_bkt_nr)
+
 
 def insert_image(arg):
-    c.bind('<Button-1>', lambda event, arg=arg: evn(event,  arg))
+    c.bind('<Button-1>', lambda event, arg=arg: evn(event, arg))
     
 
 
@@ -171,17 +273,29 @@ for i in range(0, X, GRID):
 ov = c.create_oval(-3, -3, 3, 3, tags='point', fill='red')
 
 canvas_symbols = ttk.Frame(window)
-red_circle_btn = Button(canvas_symbols, text='Red Circle', command=lambda x='red_circle': insert_image(x)).pack()
-blue_X_btn = Button(canvas_symbols, text='Blue X', command=lambda x='blue_X': insert_image(x)).pack()
-red_open_bracket_btn = Button(canvas_symbols, text='Red <', command=lambda x='red_open_bracket': insert_image(x)).pack()
-blue_close_bracket_btn = Button(canvas_symbols, text='Blue >', command=lambda x='blue_close_bracket': insert_image(x)).pack()
-red_sq_bkt_btn = Button(canvas_symbols, text='Red [', command=lambda x='red_sq_bkt': insert_image(x)).pack()
-blue_sq_bkt_btn = Button(canvas_symbols, text='Blue ]', command=lambda x='blue_sq_bkt': insert_image(x)).pack()
+red_circle_btn = Button(canvas_symbols, text='Red Circle', command=lambda x='red_circle': insert_image(x)).grid(row=2, column=0)
+blue_X_btn = Button(canvas_symbols, text='Blue X', command=lambda x='blue_X': insert_image(x)).grid(row=2, column=1)
+red_open_bracket_btn = Button(canvas_symbols, text='Red <', command=lambda x='red_open_bracket': insert_image(x)).grid(row=2, column=2)
+blue_close_bracket_btn = Button(canvas_symbols, text='Blue >', command=lambda x='blue_close_bracket': insert_image(x)).grid(row=2, column=3)
+red_sq_bkt_btn = Button(canvas_symbols, text='Red [', command=lambda x='red_sq_bkt': insert_image(x)).grid(row=2, column=4)
+blue_sq_bkt_btn = Button(canvas_symbols, text='Blue ]', command=lambda x='blue_sq_bkt': insert_image(x)).grid(row=2, column=5)
+
+# No Response
+red_circle_nr_btn = Button(canvas_symbols, text='Red Circle', command=lambda x='red_circle_nr': insert_image(x)).grid(row=3, column=0)
+blue_X_nr_btn = Button(canvas_symbols, text='Blue X', command=lambda x='blue_X_nr': insert_image(x)).grid(row=3, column=1)
+red_open_bracket_nr_btn = Button(canvas_symbols, text='Red <', command=lambda x='red_open_bracket_nr': insert_image(x)).grid(row=3, column=2)
+blue_close_bracket_nr_btn = Button(canvas_symbols, text='Blue >', command=lambda x='blue_close_bracket_nr': insert_image(x)).grid(row=3, column=3)
+red_sq_bkt_nr_btn = Button(canvas_symbols, text='Red [', command=lambda x='red_sq_bkt_nr': insert_image(x)).grid(row=3, column=4)
+blue_sq_bkt_nr_btn = Button(canvas_symbols, text='Blue ]', command=lambda x='blue_sq_bkt_nr': insert_image(x)).grid(row=3, column=5)
 def display_points():
     print(points)
     for a in points.values():
         print(len(a))
-blue_sq_bkt_btn = Button(canvas_symbols, text='get points', command=display_points).pack()
+def bind_remove_points():
+    c.unbind('<Button-1>')
+    c.bind('<Button-1>', remove_points)
+blue_sq_bkt_btn = Button(canvas_symbols, text='get points', command=display_points).grid(row=4, column=2)
+blue_sq_bkt_btn = Button(canvas_symbols, text='Remove Points', command=bind_remove_points).grid(row=4, column=3)
 canvas_symbols.grid(row=4, column=0)
 
 
--- main.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-import tkinter as tk
-from tkinter import ttk
-from Graph.graph import *
-from Graph.Globals import *
-
-window.mainloop()
